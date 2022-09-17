c194155eef7f587dda00e704a0103e35569f2e4e
WassimSlim21 <Wassimslim18@gmail.com>
2022-09-17 15:26:13 +0100

    aa

--- app.js
+++ app.js
@@ -7,11 +7,27 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const sendEmail = require("./config/sendmail");
+
 var app = express();
+
+
+/* CORS Setup*/
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  
+  next();
+});
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
+require("dotenv").config();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +37,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.post('/email', async(req, res)=>{
+try{
+  console.log("Sending mail ...");
+  await sendEmail(req.body.email, req.body.subject,req.body.content);
+  res.status(200).json({ msg: "password reset link sent to your email account" });
+}
+
+    catch (error) {
+        res.status(500).json({ err: "An error occured" });
+        console.log(error);
+    }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var userCtl = require('../controllers/users');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/',userCtl.getAllUsers);
+/* GET users listing. */
+router.post('/add',userCtl.addUser);
+/* GET users listing. */
+router.put('/update/:userId',userCtl.updateUser);
+/* GET users listing. */
+router.delete('/remove/:userId',userCtl.deleteUser);
 
 module.exports = router;
