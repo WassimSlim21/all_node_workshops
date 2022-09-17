a7aa1ed5876e57e1de6d02c8e5f01b5712e84d18
trouf <ben_aissa_mohamed@hotmail.fr>
2022-09-17 16:26:31 +0100

    3001

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.set('port', port);
   */
  
  server.listen(port, ()=>{
-    console.log('listening on :3000');
+    console.log('listening on :3001');
  });
  server.on('error', onError);
  server.on('listening', onListening);
